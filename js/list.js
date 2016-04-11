99f8e2037b9858fb131c05b9286a1566f5b07ad5
Dorian Farrimond <dorian.farrimond@gmail.com>
2016-04-11 23:53:11 +0100

    Initial commit of listings page (nearly complete)

---
+++ js/list.js
@@ -0,0 +1,52 @@
+function PhoneListViewModel(data){
+    this.id = ko.observable(data.id);
+    this.name = ko.observable(data.name);
+    this.imageUrl = ko.observable(data.imageUrl);
+    this.snippet = ko.observable(data.snippet);
+}
+
+function ListViewModel(){
+    var self = this;     
+    self.searchText = ko.observable('');
+    self.orderProp = ko.observable('name');
+    
+    self.phones = ko.observableArray([]);
+    self.phoneCount = ko.computed(function(){
+       return self.phones().length; 
+    });
+    
+    self.filterPhones = ko.computed(function(){
+        if (!self.searchText()){
+            return self.phones();
+        } else {
+            return ko.utils.arrayFilter(self.phones(), function(phone){
+                var filterText = self.searchText().toLowerCase();
+                return phone.name().toLowerCase().indexOf(filterText) > -1 ||
+                phone.snippet().toLowerCase().indexOf(filterText) > -1;
+            })
+        }
+    });
+    
+    self.querySummary = ko.computed(function(){
+        if (self.searchText() != ''){
+            return 'You searched for "' + self.searchText() + '"';            
+        }
+    });
+    
+    self.resultsSummary = ko.computed(function(){
+        if (self.phoneCount() > 0){
+            return 'Found ' + self.phoneCount() + ' results, sorted by '+ self.orderProp();
+        }
+    });
+    
+    self.pageTitle = ko.computed(function(){
+        return 'Google Phone Gallery: ' + self.searchText();
+    });    
+        
+    $.getJSON("/phones/phones.json", function(allData) {
+        var mappedPhones = $.map(allData, function(item) { return new PhoneListViewModel(item) });
+        self.phones(mappedPhones);
+    });    
+}
+
+ko.applyBindings(new ListViewModel(), document.getElementsByTagName('html')[0]);  
